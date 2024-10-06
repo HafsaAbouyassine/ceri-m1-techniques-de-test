15471927e17ba97d343d74b011b705dc2f604ef3
HafsaAbouyassine <hafsaabou2@gmail.com>
2024-10-06 17:07:58 +0200

    circl.yml valide + test unitaires Maven

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 public class MyTest {
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+        int result = 10/2;
+        assertEquals(5, result);
+    }
 }
